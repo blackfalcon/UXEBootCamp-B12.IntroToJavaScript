b92e4f723bc7dea39fff5a1be702da8bde719271
Aaron Brown <aaron.kaoru.brown@gmail.com>
2014-03-24 18:04:25 -0700

    Functionality works now

--- aaron-brown-b12/myscript.js
+++ aaron-brown-b12/myscript.js
@@ -1,4 +1,6 @@
 
+var allRolls = [];
+
 var diceRoll = function(diceSides, diceNumber) {
   var nResult = 0;
   diceSides = Math.floor(diceSides);
@@ -7,8 +9,12 @@ var diceRoll = function(diceSides, diceNumber) {
     for(n = 0; n < diceNumber; n++){
       nResult = nResult + (Math.ceil(Math.random() * diceSides));
     }
+    alert("Your roll:" + nResult);
+    allRolls.push(nResult);
+    alert("Your total rolls:" + allRolls);
+    return nResult;
   } else {
     alert("Invalid input!!!");
+    return nResult;
   }
-  return nResult;
 }
