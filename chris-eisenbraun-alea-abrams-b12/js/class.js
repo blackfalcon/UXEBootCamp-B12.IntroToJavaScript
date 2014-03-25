10590cec75ea13c9ee1dafdbcfd293af3e726431
Chris Eisenbraun <hello@chriseisenbraun.com>
2014-03-25 14:33:05 -0700

    Add all the directories

---
+++ chris-eisenbraun-alea-abrams-b12/js/class.js
@@ -0,0 +1,6 @@
+var age = prompt("You must be 13 or older to play. Enter your age.");
+    if (age >= 13) {
+      alert("Welcome brave soul, your nightmare awaits!!!");
+    } else {
+      alert("Whoa there! Seems you are a bit young to be playing this game.");
+    }
